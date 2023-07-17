0b0558b739ea1801c53d0fb358332b098460de72
Seongwhwan Park <joungjaehyun1008@gmail.com>
2023-07-17 10:23:17 +0900

    Member Step-1

---
+++ src/main/java/org/zerock/j2/dto/MemberDTO.java
@@ -0,0 +1,19 @@
+package org.zerock.j2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// JSON으로 데이터를 보내기위한 DTO
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDTO {
+
+    private  String email;
+    private  String pw;
+    private  String nickname;
+    private  boolean admin;
+}
