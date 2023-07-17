be30eda6f81e9001255dac78922605b949736de4
Seongwhwan Park <joungjaehyun1008@gmail.com>
2023-07-17 15:08:29 +0900

    Cart Server

--- src/main/java/org/zerock/j2/dto/MemberCartDTO.java
+++ src/main/java/org/zerock/j2/dto/MemberCartDTO.java
@@ -1,6 +1,14 @@
 package org.zerock.j2.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberCartDTO {
 
     private  Long cno;
---
+++ src/main/java/org/zerock/j2/service/MemberCartService.java
@@ -0,0 +1,36 @@
+package org.zerock.j2.service;
+
+import jakarta.transaction.Transactional;
+import org.zerock.j2.dto.MemberCartDTO;
+import org.zerock.j2.entity.MemberCart;
+
+import java.util.List;
+
+@Transactional
+public interface MemberCartService {
+
+    List<MemberCartDTO> addCart(MemberCartDTO memberCartDTO);
+
+    default MemberCart dtoToEntity(MemberCartDTO dto) {
+
+        MemberCart entity = MemberCart.builder()
+                .cno(dto.getCno())
+                .email(dto.getEmail())
+                .pno(dto.getPno())
+                .build();
+
+        return entity;
+    }
+
+    default  MemberCartDTO entityToDTO(MemberCart entity){
+
+        MemberCartDTO dto = MemberCartDTO.builder()
+                .cno(entity.getCno())
+                .email(entity.getEmail())
+                .pno(entity.getPno())
+                .build();
+
+        return dto;
+    }
+
+}
